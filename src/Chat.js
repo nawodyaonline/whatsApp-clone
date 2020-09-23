64cdefb77034b8e386a7d707bff3126aad22069b
nawodyaonline <nawodyahckr@gmail.com>
2020-09-23 21:24:08 +0530

    Add chatbox

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,55 @@
-import React from 'react'
-import './Chat.css'
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./Chat.css";
 
 function Chat() {
-    return (
-        <div className="chat">
-            <h1>I am the Chat box</h1>
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-    )
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat_body">
+        <p className="chat_message">
+          <span className="chat_name">Nawodya</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat_message chat_reciever">
+          <span className="chat_name">Nawodya</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat_message">
+          <span className="chat_name">Nawodya</span>
+          This is a message
+          <span className="chat_timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
